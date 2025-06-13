2229a9c657d4ee9261ccea3f9034ad4db3cfd66a
Juan Pablo Gómez <thebrimstone1@gmail.com>
2025-06-13 15:21:08 -0500

    feat: Added tracker feature with Supervision, initiation of circle tracking

---
+++ main.py
@@ -0,0 +1,19 @@
+from utils import save_video, read_video
+from tracker import Tracker
+
+def main():
+    #Read Video
+    video_frames = read_video('input_videos/08fd33_4.mp4')
+    
+    #Initialize Tracker
+    tracker = Tracker('models/best.pt')
+    
+    tracks = tracker.get_object_tracks(video_frames,
+                                       read_from_stub=True,
+                                       stub_path='stubs/track_stubs.pkl')
+    
+    #Save video
+    save_video(video_frames, 'output_videos/output_video.avi')
+    
+if __name__ == '__main__':
+    main()
