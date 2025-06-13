15f347db5f03d1e08efab7620b5aee14a40c0955
Juan Pablo Gómez <thebrimstone1@gmail.com>
2025-06-13 00:19:10 -0500

    feat: Created model with yolo and fine-tuned with yolo for players, referees and ball tracking and boxing

---
+++ yolo_inferenceft.py
@@ -0,0 +1,11 @@
+from ultralytics import YOLO
+
+model = YOLO('models/best.pt')
+
+results = model.predict('input_videos/08fd33_4.mp4',save=True) #Your video here
+print(results[0]) #first frame results
+
+print('=====================================')
+
+for box in results[0].boxes:
+    print(box)
