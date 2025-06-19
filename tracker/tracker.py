f01f7c7fb5239484b03ed8b07af848791d2f706d
Juan Pablo Gómez <thebrimstone1@gmail.com>
2025-06-19 15:09:20 -0500

    feat: Added rectangles on trackers to keep count of players

--- tracker/tracker.py
+++ tracker/tracker.py
@@ -82,7 +82,7 @@ class Tracker:
                     
         return tracks        
  
-    def draw_ellipse(self, frame,bbox, color, track_id):
+    def draw_ellipse(self, frame,bbox, color, track_id=None):
         y2 = int(bbox[3])
         x_center, _ = get_center_of_bbox(bbox)
         width = get_bbox_width(bbox)
@@ -92,13 +92,41 @@ class Tracker:
             center = (x_center, y2),
             axes = (int(width), int(0.35*width)),
             angle = 0.0,
-            startAngle = 45,
+            startAngle = -45,
             endAngle = 235,
             color = color,
             thickness = 2,
             lineType = cv2.LINE_4
         )
         
+        rectangle_width = 40
+        rectangle_height = 20
+        x1_rect = x_center - rectangle_width//2
+        x2_rect = x_center + rectangle_width//2
+        y1_rect = (y2 - rectangle_height//2) + 15
+        y2_rect = (y2 + rectangle_height//2) + 15
+        
+        if track_id is not None:
+            cv2.rectangle(frame,
+                          (int(x1_rect), int(y1_rect)),
+                          (int(x2_rect), int(y2_rect)),
+                          color,
+                          cv2.FILLED)
+            
+            x1_text = x1_rect + 12
+            if track_id > 99:
+                x1_text -= 10
+            
+            cv2.putText(
+                frame,
+                f"{track_id}",
+                (int(x1_text), int(y1_rect + 15)),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.6,
+                (0,0,0),
+                2
+            )
+
         return frame
                 
     def draw_anotations(self, video_frames, tracks):
@@ -113,6 +141,10 @@ class Tracker:
             #Draw Players
             for track_id, player in player_dict.items():
                 frame = self.draw_ellipse(frame, player["bbox"], (0,0,255), track_id)
+                
+            #Draw Referees
+            for _, referee in referee_dict.items():
+                frame = self.draw_ellipse(frame, referee["bbox"], (0,255,255))
                 
             output_video_frames.append(frame)
         
