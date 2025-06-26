0146c6340c55f0e4052d9f1fd3ecf25c9acb577a
Juan Pablo Gómez <thebrimstone1@gmail.com>
2025-06-26 01:07:30 -0500

    feat: cropped image created

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import cv2
 from utils import save_video, read_video
 from tracker import Tracker
 
@@ -12,6 +13,18 @@ def main():
                                        read_from_stub=True,
                                        stub_path='stubs/track_stubs.pkl')
  
+    #Save cropped image of player
+    for track_id, player in tracks['players'][0].items():
+        bbox = player['bbox']
+        frame = video_frames[0]
+        
+        #Crop bbox from frame
+        cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+
+        #Save the cropped image
+        cv2.imwrite(f'output_videos/cropped_image.jpg', cropped_image)
+        break
+    
     #Draw Output
     ##Draw object Tracks
     output_video_frames = tracker.draw_anotations(video_frames, tracks)
