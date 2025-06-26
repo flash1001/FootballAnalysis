b46b7b9af001bda3405e424efead8a817273b235
Juan Pablo Gómez <thebrimstone1@gmail.com>
2025-06-26 14:40:41 -0500

    feat: created player color assignement with k-means to get player color, assign team color and get player team

--- main.py
+++ main.py
@@ -1,35 +1,40 @@
 import cv2
 from utils import save_video, read_video
 from tracker import Tracker
+from team_assigner import TeamAssigner
 
 def main():
-    #Read Video
+    # Read Video
     video_frames = read_video('input_videos/08fd33_4.mp4')
     
-    #Initialize Tracker
+    # Initialize Tracker
     tracker = Tracker('models/best.pt')
     
     tracks = tracker.get_object_tracks(video_frames,
                                        read_from_stub=True,
                                        stub_path='stubs/track_stubs.pkl')
  
-    #Save cropped image of player
-    for track_id, player in tracks['players'][0].items():
-        bbox = player['bbox']
-        frame = video_frames[0]
-        
-        #Crop bbox from frame
-        cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
-
-        #Save the cropped image
-        cv2.imwrite(f'output_videos/cropped_image.jpg', cropped_image)
-        break
+    # Assign Player Teams
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], 
+                                    tracks['players'][0])
     
-    #Draw Output
-    ##Draw object Tracks
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            
+            team = team_assigner.get_player_team(video_frames[frame_num],   
+                                                 track['bbox'],
+                                                 player_id)
+            
+            tracks['players'][frame_num][player_id]['team'] = team 
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+    
+    
+    # Draw Output
+    ## Draw object Tracks
     output_video_frames = tracker.draw_anotations(video_frames, tracks)
     
-    #Save video
+    # Save video
     save_video(output_video_frames, 'output_videos/output_video.avi')
     
 if __name__ == '__main__':
