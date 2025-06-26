176af28051f542a625cb5d16048c5a2f909d241d
Juan Pablo Gómez <thebrimstone1@gmail.com>
2025-06-26 18:27:06 -0500

    feat: Tracker now tracks players with posession of the ball; posession percentages yet to be developed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 from utils import save_video, read_video
 from tracker import Tracker
 from team_assigner import TeamAssigner
+from player_ball_assigner import PlayerBallAssigner
 
 def main():
     # Read Video
@@ -32,7 +33,16 @@ def main():
             tracks['players'][frame_num][player_id]['team'] = team 
             tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
     
-    
+    # Assign Ball Aquisition
+    player_assigner = PlayerBallAssigner()
+    team_ball_control= []
+    for frame_num, player_track in enumerate(tracks['players']):
+        ball_bbox = tracks['ball'][frame_num][1]['bbox']
+        assigned_player = player_assigner.assign_ball_to_player(player_track, ball_bbox)
+
+        if assigned_player != -1:
+            tracks['players'][frame_num][assigned_player]['has_ball'] = True
+             
     # Draw Output
     ## Draw object Tracks
     output_video_frames = tracker.draw_anotations(video_frames, tracks)
