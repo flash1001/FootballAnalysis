373370ff34a6188d0c6e680c651756808931bbac
Juan Pablo Gómez <thebrimstone1@gmail.com>
2025-07-04 12:19:39 -0500

    feat: Created speed and distance estimator for each player

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from player_ball_assigner import PlayerBallAssigner
 from camera_movement_estimator import CameraMovementEstimator
 import numpy as np
 from view_transformer import ViewTransformer
+from speed_and_distance_estimator import SpeedAndDistance_Estimator
 
 def main():
     # Read Video
@@ -35,6 +36,10 @@ def main():
     
     # Interpolate Ball Positions
     tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
+    
+    #Speed and distance estimator
+    speed_and_distance_estimator = SpeedAndDistance_Estimator()
+    speed_and_distance_estimator.add_speed_and_distance_to_tracks(tracks)
     
     # Assign Player Teams
     team_assigner = TeamAssigner()
@@ -72,6 +77,9 @@ def main():
     
     ##Draw camera movement
     output_video_frames = camera_movement_estimator.draw_camera_movement(output_video_frames, camera_movement_per_frame)
+    
+    ##Draw speed and distance
+    speed_and_distance_estimator.draw_speed_and_distance(output_video_frames, tracks)
     
     # Save video
     save_video(output_video_frames, 'output_videos/output_video.avi')
